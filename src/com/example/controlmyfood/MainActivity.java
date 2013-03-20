21e7790efcda27ad85e53f509da151c14a47808a
luizandia <andia.luiz@gmail.com>
2013-03-20 01:24:18 -0300

    tem botap agora

--- src/com/example/controlmyfood/MainActivity.java
+++ src/com/example/controlmyfood/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.controlmyfood;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	// private Button botaoTeste;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		/*
+		 * botaoTeste = (Button) findViewById(R.id.button1);
+		 * 
+		 * botaoTeste.setOnClickListener(new View.OnClickListener() {
+		 * 
+		 * @Override public void onClick(View v) {
+		 * Toast.makeText(BasicButtonActivity.this, "Button clicked!",
+		 * Toast.LENGTH_SHORT).show(); } });
+		 */
+
 		Log.d("debug", "Fim");
 		Log.d("debug", "tripinhaaa");
 		Log.d("debug", "Teste");
+	}
+
+	public void Button1Bt1(View v) {
+		System.out.println("VAMO VAMO VAMO");
+		Toast.makeText(this, "Produto Cadastrado", Toast.LENGTH_SHORT).show();
+
 	}
 
 	@Override
